6d8ca910923382391e9540fb267199d06a084b5b
Tim Ousborne <dev@sourbone.net>
2019-05-30 00:35:49 -0600

    refactor: format using rustfmt.toml

--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,34 @@
 
 // Standard libraries
 use amethyst::{
-    core::transform::TransformBundle,
-    prelude::*,
-    renderer::{DisplayConfig, DrawFlat2D, Pipeline, RenderBundle, Stage},
-    utils::application_root_dir,
+	core::transform::TransformBundle,
+	prelude::*,
+	renderer::{DisplayConfig, DrawFlat2D, Pipeline, RenderBundle, Stage},
+	utils::application_root_dir,
 };
 
 // Project libraries
 mod pong;
 
 fn main() -> amethyst::Result<()> {
-    amethyst::start_logger(Default::default());
+	amethyst::start_logger(Default::default());
 
-    let path = format!("{}/resources/display_config.ron", application_root_dir());
-    let config = DisplayConfig::load(&path);
+	let path = format!("{}/resources/display_config.ron", application_root_dir());
+	let config = DisplayConfig::load(&path);
 
-    // Clear the screen to IKB.
-    let pipe = Pipeline::build().with_stage(
-        Stage::with_backbuffer()
-            .clear_target([0.0, 0.18, 0.65, 2.0], 1.0)
-            .with_pass(DrawFlat2D::new()),
-    );
+	// Clear the screen to IKB.
+	let pipe = Pipeline::build().with_stage(
+		Stage::with_backbuffer()
+			.clear_target([0.0, 0.18, 0.65, 2.0], 1.0)
+			.with_pass(DrawFlat2D::new()),
+	);
 
-    let game_data = GameDataBuilder::default()
-        .with_bundle(RenderBundle::new(pipe, Some(config))
-                     .with_sprite_sheet_processor())?
-        .with_bundle(TransformBundle::new())?;
+	let game_data = GameDataBuilder::default()
+		.with_bundle(RenderBundle::new(pipe, Some(config)).with_sprite_sheet_processor())?
+		.with_bundle(TransformBundle::new())?;
 
-    let mut game = Application::new("./", pong::Pong, game_data)?;
-    game.run();
+	let mut game = Application::new("./", pong::Pong, game_data)?;
+	game.run();
 
-    return Ok(());
+	return Ok(());
 }
--- src/pong.rs
+++ src/pong.rs
@@ -2,14 +2,22 @@
 
 // Standard libraries
 use amethyst::{
-    assets::{AssetStorage, Loader},
-    core::transform::Transform,
-    ecs::prelude::{Component, DenseVecStorage},
-    prelude::*,
-    renderer::{
-        Camera, Flipped, PngFormat, Projection, SpriteRender, SpriteSheet,
-        SpriteSheetFormat, SpriteSheetHandle, Texture, TextureMetadata
-    },
+	assets::{AssetStorage, Loader},
+	core::transform::Transform,
+	ecs::prelude::{Component, DenseVecStorage},
+	prelude::*,
+	renderer::{
+		Camera,
+		Flipped,
+		PngFormat,
+		Projection,
+		SpriteRender,
+		SpriteSheet,
+		SpriteSheetFormat,
+		SpriteSheetHandle,
+		Texture,
+		TextureMetadata,
+	},
 };
 
 // Constants
@@ -21,8 +29,8 @@ const PADDLE_WIDTH: f32 = 4.0;
 // Enumeration of sides of the game world.
 #[derive(PartialEq, Eq)]
 pub enum Side {
-    Left,
-    Right
+	Left,
+	Right,
 }
 
 // The game state.
@@ -30,103 +38,97 @@ pub struct Pong;
 
 // SimpleState implements a minimal state machine
 impl SimpleState for Pong {
-    fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
-        let world = data.world;
-        let sprite_sheet = load_sprite_sheet(world);
+	fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
+		let world = data.world;
+		let sprite_sheet = load_sprite_sheet(world);
 
-        world.register::<Paddle>();
+		world.register::<Paddle>();
 
-        initialize_paddles(world, sprite_sheet);
-        initialize_camera(world);
-    }
+		initialize_paddles(world, sprite_sheet);
+		initialize_camera(world);
+	}
 }
 
 // Represents a player controlled paddle.
 pub struct Paddle {
-    pub side: Side,
-    pub width: f32,
-    pub height: f32,
+	pub side: Side,
+	pub width: f32,
+	pub height: f32,
 }
 
 impl Paddle {
-    fn new(side: Side) -> Paddle {
-        return Paddle {
-            side: side,
-            width: 1.0,
-            height: 1.0,
-        }
-    }
+	fn new(side: Side) -> Paddle {
+		return Paddle { side: side, width: 1.0, height: 1.0 };
+	}
 }
 
 // The paddle is associated with the game engine and the storage type defined.
 impl Component for Paddle {
-    type Storage = DenseVecStorage<Self>;
+	type Storage = DenseVecStorage<Self>;
 }
 
 // Add the initial cameras to the world.
 fn initialize_camera(world: &mut World) {
-    let mut transform = Transform::default();
-    transform.set_z(1.0);
+	let mut transform = Transform::default();
+	transform.set_z(1.0);
 
-    world
-        .create_entity()
-        .with(Camera::from(Projection::orthographic(0.0, ARENA_WIDTH, 0.0, ARENA_HEIGHT)))
-        .with(transform)
-        .build();
+	world
+		.create_entity()
+		.with(Camera::from(Projection::orthographic(0.0, ARENA_WIDTH, 0.0, ARENA_HEIGHT)))
+		.with(transform)
+		.build();
 }
 
 // Add the initial paddles to the world.
 fn initialize_paddles(world: &mut World, sprite_sheet: SpriteSheetHandle) {
-    let mut left_transform = Transform::default();
-    let mut right_transform = Transform::default();
+	let mut left_transform = Transform::default();
+	let mut right_transform = Transform::default();
 
-    let y = ARENA_HEIGHT * 0.5;
-    left_transform.set_xyz(PADDLE_WIDTH * 0.5, y, 0.0);
-    right_transform.set_xyz(ARENA_WIDTH - PADDLE_WIDTH * 0.5, y, 0.0);
+	let y = ARENA_HEIGHT * 0.5;
+	left_transform.set_xyz(PADDLE_WIDTH * 0.5, y, 0.0);
+	right_transform.set_xyz(ARENA_WIDTH - PADDLE_WIDTH * 0.5, y, 0.0);
 
-    let sprite_render = SpriteRender {
-        sprite_sheet: sprite_sheet.clone(),
-        sprite_number: 0,
-    };
+	let sprite_render = SpriteRender { sprite_sheet: sprite_sheet.clone(), sprite_number: 0 };
 
-    // Add the left paddle to the world.
-    world
-        .create_entity()
-        .with(sprite_render.clone())
-        .with(Paddle::new(Side::Left))
-        .with(left_transform)
-        .build();
+// Add the left paddle to the world.
+	world
+		.create_entity()
+		.with(sprite_render.clone())
+		.with(Paddle::new(Side::Left))
+		.with(left_transform)
+		.build();
 
-    // Add the right paddle to the world.
-    world
-        .create_entity()
-        .with(sprite_render.clone())
-        .with(Flipped::Horizontal)
-        .with(Paddle::new(Side::Right))
-        .with(right_transform)
-        .build();
+	// Add the right paddle to the world.
+	world
+		.create_entity()
+		.with(sprite_render.clone())
+		.with(Flipped::Horizontal)
+		.with(Paddle::new(Side::Right))
+		.with(right_transform)
+		.build();
 }
 
+// Load the sprite sheets.
 fn load_sprite_sheet(world: &mut World) -> SpriteSheetHandle {
-    let texture_handle = {
-        let loader = world.read_resource::<Loader>();
-        let texture_storage = world.read_resource::<AssetStorage<Texture>>();
-        loader.load(
-            "texture/pong_spritesheet.png",
-            PngFormat,
-            TextureMetadata::srgb_scale(),
-            (),
-            &texture_storage,
-        )
-    };
+	let texture_handle = {
+		let loader = world.read_resource::<Loader>();
+		let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+		loader.load(
+			"texture/pong_spritesheet.png",
+			PngFormat,
+			TextureMetadata::srgb_scale(),
+			(),
+			&texture_storage,
+		)
+	};
 
-    let loader = world.read_resource::<Loader>();
-    let sprite_sheet_store = world.read_resource::<AssetStorage<SpriteSheet>>();
-    loader.load(
-        "texture/pong_spritesheet.ron",
-        SpriteSheetFormat,
-        texture_handle,
-        (),
-        &sprite_sheet_store,
-    )
+	let loader = world.read_resource::<Loader>();
+	let sprite_sheet_store = world.read_resource::<AssetStorage<SpriteSheet>>();
+	loader.load(
+		"texture/pong_spritesheet.ron",
+		SpriteSheetFormat,
+		texture_handle,
+		(),
+		&sprite_sheet_store,
+	)
 }
